5320eb0e042130adcaa22d9d0e3701068061a2d4
LikerK <nagorniykirill@gmail.com>
2022-11-16 15:34:33 +0400

    add brain-calc

---
+++ bin/brain-calc.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+import * as game from '../games/brain-calc.js';
+import executor from '../src/index.js';
+
+executor(game);
---
+++ games/brain-calc.js
@@ -0,0 +1,23 @@
+export const task = 'What is the result of the expression?';
+const operations = ['+', '-', '*'];
+const maxNumber = 100;
+
+export const getRound = () => {
+  const randomIndex = Math.floor(Math.random() * (operations.length - 1));
+  const firstNumber = Math.trunc(Math.random() * maxNumber);
+  const secondNumber = Math.trunc(Math.random() * maxNumber);
+  const operation = operations[randomIndex];
+  if (operation === '+') {
+    const answer = firstNumber + secondNumber;
+    const expression = `${firstNumber} + ${secondNumber}`;
+    return [expression, String(answer)];
+  }
+  if (operation === '-') {
+    const answer = firstNumber - secondNumber;
+    const expression = `${firstNumber} - ${secondNumber}`;
+    return [expression, String(answer)];
+  }
+  const answer = firstNumber * secondNumber;
+  const expression = `${firstNumber} * ${secondNumber}`;
+  return [expression, String(answer)];
+};
